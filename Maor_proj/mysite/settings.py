49c9ceabb9685de49f095dd039525edb39c053bc
Maorb23 <146326556+Maorb23@users.noreply.github.com>
2024-10-24 02:02:25 +0300

    Update settings.py

--- Maor_proj/mysite/settings.py
+++ Maor_proj/mysite/settings.py
@@ -24,8 +24,8 @@ CSRF_TRUSTED_ORIGINS = ['https://mysite-mocl.onrender.com']
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+#DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
 
