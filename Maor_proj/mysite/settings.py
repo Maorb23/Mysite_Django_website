44d4c57aa83417ca75d21b26be69f45d64f4ed1c
Maorb23 <146326556+Maorb23@users.noreply.github.com>
2025-01-01 14:28:06 +0200

    Update settings.py

--- Maor_proj/mysite/settings.py
+++ Maor_proj/mysite/settings.py
@@ -119,14 +119,14 @@ import dj_database_url
 
 #DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 
 # Password validation
