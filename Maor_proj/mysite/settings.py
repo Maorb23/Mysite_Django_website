f92011dc22dd34966ea3a8e1018a95d05be8da12
Maorb23 <146326556+Maorb23@users.noreply.github.com>
2025-01-01 14:47:38 +0200

    Update settings.py

--- Maor_proj/mysite/settings.py
+++ Maor_proj/mysite/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
 # run with waitress: waitress-serve --port=8000 Maor_proj.mysite.wsgi:application
 import dj_database_url
 database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
+#DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
 
 #DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
